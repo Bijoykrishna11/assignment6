e65d413a6586e5851493a822b3aee0ff508c7f67
Bijoy <hskbijoy123@gmail.com>
2022-09-04 04:01:48 +0600

    blog modal

--- js/assignment.js
+++ js/assignment.js
@@ -30,61 +30,60 @@
 
 
 
-//  ................... end...............//
+// //  ................... end...............//
 
 
-/*
 
-const allNews = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
-    const Response = await fetch(url);
-    const data = await Response.json();
-    displayItem(data.data);
-}
 
-const displayItem = khabor => {
-    // console.log(khabor);
+// const allNews = async () => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/01`
+//     const Response = await fetch(url);
+//     const data = await Response.json();
+//     displayItem(data.data);
+// }
 
-    const newsContainer = document.getElementById('news-container');
+// const displayItem = khabor => {
+//     // console.log(khabor);
 
-    khabor.forEach(element => {
-        const newsDiv = document.createElement('div');
-        newsDiv.classList.add('coll');
-        newsDiv.innerHTML = `
-                    <div class="card mb-4  ">
-                            <img src="${element.image_url}" class="  card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${element.title}</h5>
-                                <p class="card-text">${element.details} </p>
+//     const newsContainer = document.getElementById('news-container');
+
+//     khabor.forEach(element => {
+//         const newsDiv = document.createElement('div');
+//         newsDiv.classList.add('coll');
+//         newsDiv.innerHTML = `
+//                     <div class="card mb-4  ">
+//                             <img src="${element.image_url}" class="  card-img-top" alt="...">
+//                             <div class="card-body">
+//                                 <h5 class="card-title">${element.title}</h5>
+//                                 <p class="card-text">${element.details} </p>
                                               
-                                <div class="d-flex">
-                                <img src="${element.author.img}" class="w-25  card-img-top" alt="...">
-                                <p class="card-text fw-bolder fs-2 mt-5 ms-5">${element.author.name} </p>     
-                                <p class="card-text fs-3 mt-5 ms-5">${element.total_view} views 
-                                <span><button onclick="getDetail()" class="bg-primary ms-5 fs-4 text-light border border-danger rounded-3">More detals</button></span> </p>
-                            </div>                
-                    </div> `;
+//                                 <div class="d-flex">
+//                                 <img src="${element.author.img}" class="w-25  card-img-top" alt="...">
+//                                 <p class="card-text fw-bolder fs-2 mt-5 ms-5">${element.author.name} </p>     
+//                                 <p class="card-text fs-3 mt-5 ms-5">${element.total_view} views 
+//                                 <span><button onclick="getDetail()" class="bg-primary ms-5 fs-4 text-light border border-danger rounded-3">More detals</button></span> </p>
+//                             </div>                
+//                     </div> `;
 
-        newsContainer.appendChild(newsDiv);
+//         newsContainer.appendChild(newsDiv);
 
-    })
+//     })
 
-}
-const getDetail = () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/{category_id}`
+// }
+// const getDetail = () => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/{category_id}`
 
-    fetch(url)
-        .then(res => res.json())
-        .then(datas => displayDitles(datas))
-}
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(datas => displayDitles(datas))
+// }
 
-const displayDitles = sports => {
-    console.log(sports);
+// const displayDitles = sports => {
+//     console.log(sports);
 
-}
+// }
 
-allNews()
+// allNews()
 
 
-*/
 
