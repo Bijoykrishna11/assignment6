beb785ead3473cb47715b0afda383950c628a16a
Bijoy <hskbijoy123@gmail.com>
2022-09-03 14:49:34 +0600

    example part

--- js/assignment.js
+++ js/assignment.js
@@ -20,7 +20,7 @@
 
 
 
-//   end
+//  ................... end...............//
 
 
 const allNews = async () => {
@@ -31,10 +31,39 @@ const allNews = async () => {
 }
 
 const displayItem = khabor => {
-    console.log(khabor);
+    // console.log(khabor);
+
+    const newsContainer = document.getElementById('news-container');
+
+    khabor.forEach(element => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('coll');
+        newsDiv.innerHTML = `
+                    <div class="card mb-4  ">
+                            <img src="${element.image_url}" class="  card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${element.title}</h5>
+                                <p class="card-text">${element.details} </p>
+                            
+                                
+                                
+        <div class="d-flex">
+        <img src="${element.author.img}" class="w-25  card-img-top" alt="...">
+        <p class="card-text fw-bolder fs-2 mt-5 ms-5">${element.author.name} </p>     
+        <p class="card-text fs-3 mt-5 ms-5">${element.total_view} views 
+        <span><button class="bg-primary ms-5 fs-4 text-light border border-danger rounded-3">More detals</button></span> </p>
+        
+     
+    </div>
+                 
+                        </div>
+                        `;
+
+        newsContainer.appendChild(newsDiv);
+
+    })
+
 }
-
-
 allNews()
 
 
